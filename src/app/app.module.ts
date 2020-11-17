ce3334fe08f46e55c3c54f247d86b671c5252373
Przemek Żbikowski <you@example.com>
2020-11-17 14:24:32 +0100

    - created Core and Shared modules - created Header component in shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CarsModule } from './cars/cars.module';
 import { CarsService } from './cars/cars.service';
+import { CoreModule} from './core-module/core.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { CarsService } from './cars/cars.service';
   imports: [
     BrowserModule,
     CarsModule,
+    CoreModule,
     HttpClientModule
   ],
   providers: [CarsService],
