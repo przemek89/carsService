50b0c288251a79321a42f89aef496c162e2a90ec
Przemek Żbikowski <you@example.com>
2020-11-26 11:19:11 +0100

    - added a pipe to trnasform surname into a single letter

--- src/app/shared-module/shared.module.ts
+++ src/app/shared-module/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SurnameShortcutPipe } from './pipes/surname-shortcut.pipe';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SurnameShortcutPipe],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, SurnameShortcutPipe]
 })
 export class SharedModule { }
